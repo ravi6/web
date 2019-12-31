2b516a5934c4930eddee0c5a667d255b6b3f0152
Ravi <ravi_saripalli@hotmail.com>
2019-12-31 20:47:55 +1100

    First Version

---
+++ funcs.js
@@ -0,0 +1,138 @@
+/* Keep all scripts here
+Author: Ravi Saripalli
+Date:   31st Dec. 2019
+*/
+// Yukky Globals
+var rag1="aabhogi", rag2="aarabhi" , count=-1;
+
+function capFirst(str) {
+    str = str.split(" ");
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    }
+    return str.join(" ");
+} // capFirst
+
+   function selRaga (sel) { //Display selected raga
+     // Storing last two raga selections
+     count = count + 1 ;
+     if (count > 1) count = 0 ;
+     if (count == 0)
+       rag1 = sel.innerHTML ;
+     else
+      rag2 = sel.innerHTML ;
+
+     let raga = findRaga(sel.innerHTML);
+     let buf = [] ;
+     raga.kritis.forEach (function (elm) {
+                  buf.push ("<li>" + elm + "</li>");
+                   });
+     document.getElementById("klist").innerHTML = buf.join("");
+     document.getElementById("ragam").innerHTML = capFirst(raga.name) ;
+
+     let txt = "<ul><li>Aarohanam: " 
+              + raga.notes.aarohana + "</li>"
+              + "<li>Avarohanam: " + raga.notes.avarohana
+              + "</li></ul>";
+     document.getElementById("notes").innerHTML = txt ;
+  } // end selRaga
+
+   function findRaga (name) {// pick a raga object
+    let raga = {};
+    for (let i=0 ; i < obj.ragas.length ; i++) {
+       if ( obj.ragas[i].name === name){
+          raga = obj.ragas[i] ; 
+	  break ;
+       }
+    }
+     return (raga);
+   } // end findRaga 
+
+   function getRagas() { // Collate all ragams I know of
+     ragams = [] ;
+     obj.ragas.forEach ( function (ragam) { ragams.push(ragam.name) ; } );
+     return (ragams.sort());
+   } // end getRagas
+
+    function dropListRagas() { // adds Ragas to drodownlist
+      let buf = [] ;
+      let ragas = getRagas() ;
+
+      for (let k=0; k < ragas.length ; k++) {
+	  let name = ragas[k] ;
+	  buf.push ("<a class='dropdown-item' onclick='" 
+	           +  "selRaga(this);" + "'>" + name + "</a>");
+	  }
+      document.getElementById("menuRaga").innerHTML = buf.join("") ;
+      // console.log(document.getElementById("menuRaga").innerHTML);
+
+    } // end dropListRagas
+
+    function showStats() { // of interest to me
+      let buf = [] ;
+      let ragas = getRagas() ;
+      let nk = 0 ;
+      obj.ragas.forEach ( function (ragam) { 
+	                   nk = nk + ragam.kritis.length ; } );
+
+      document.getElementById("nragas").innerHTML = "Ragas: " + ragas.length ;
+      document.getElementById("nkritis").innerHTML = "Kritis: " + nk ;
+    } // end showStats
+
+    function listKritis() { // show matched Kritis
+      let buf = [] ;
+      let ragas = getRagas() ;
+      let astr = document.getElementById('ss').value;
+      if(astr.trim()==="" || astr === null || astr === undefined) {
+	return ;
+      }
+      
+     buf.push("<div class='col'> <ul>");
+      astr = astr.toUpperCase();
+      for (let k=0; k < ragas.length ; k++) {
+	let name = ragas[k] ;
+	for (let j=0; j < obj.ragas[k].kritis.length ; j++) {
+	  let kriti = obj.ragas[k].kritis[j];
+	  if (kriti.toUpperCase().search(astr) != -1) { // if match
+	    buf.push ("<li>" + kriti + " -> " + "<a href='#' onclick='" 
+	      +  "selRaga(this);" + "'>" + name + "</a></li>");
+	  }
+	}
+      }
+     buf.push("</ul></div>");
+      document.getElementById("output").innerHTML = buf.join("") ;
+      document.getElementById("outputTitle").innerHTML = "Kirtana Search Result" ;
+
+    } // end listKirtanas
+
+    function compRagas() {
+      let i1, i2 ;
+      let buf = [] ;
+      console.log(rag1, rag2);
+      let ragas = getRagas() ;
+        for (let i=0 ; i < ragas.length ; i++) {
+	    if (obj.ragas[i].name == rag1)
+	       i1 = i;
+	    else if (obj.ragas[i].name == rag2)
+	       i2 = i;
+	}
+      buf.push("<table class='table table-striped table-condensed'><tbody>")
+      buf.push("<tr>"); buf.push("<td>  </td>");
+      buf.push("<td>Aarohana</td>");
+      buf.push("<td>Avarohana</td>");
+      buf.push("/<tr>");
+      buf.push("<tr>"); buf.push("<td>" + rag1 + "</td>");
+      buf.push("<td>" + obj.ragas[i1].notes.aarohana + "</td>");
+      buf.push("<td>" + obj.ragas[i1].notes.avarohana + "</td>");
+      buf.push("/<tr>");
+      buf.push("<tr>"); buf.push("<td>" + rag2 + "</td>");
+      buf.push("<td>" + obj.ragas[i2].notes.aarohana + "</td>");
+      buf.push("<td>" + obj.ragas[i2].notes.avarohana + "</td>");
+      buf.push("/<tr>");
+      buf.push("</tbody></table>")
+      document.getElementById("output").innerHTML = buf.join("") ;
+      document.getElementById("outputTitle").innerHTML = "Comparison" ;
+    }
+
+     $(document).ready (function (){dropListRagas(); 
+                                    showStats();}) ;
---
+++ ragas.js
@@ -0,0 +1,613 @@
+/* Keep all scripts here
+Author: Ravi Saripalli
+Date:   31st Dec. 2019
+*/
+var obj = {
+  "ragas": [
+    {
+      "name": "suddha saveri",
+      "kritis": [
+        "NaaraaYanaTe NamoNamo"
+      ],
+      "notes": {
+        "aarohana": "S P A S A RI G2",
+        "avarohana": "R G M P D N S"
+      }
+    },
+
+    {
+      "name": "karaha priya",
+      "kritis": [
+        "Okapari Kokaparia",
+        "Prakkala nilabadia",
+        "Janakee pathe jaya kaarunya"
+      ],
+      "notes": {
+        "aarohana":  "S R2 G2 M1 P D2 N2 S",
+        "avarohana": "S N2 D2 P M1 G2 R2 S"
+      }
+    },
+
+    {
+      "name": "abheri",
+      "kritis": [
+      "Nagumomu kanaleni"
+      ],
+      "notes": {
+        "aarohana": "S G2 M1 P N2 S",
+        "avarohana": "S N2 D2 P M1 G2 R2 S"
+      }
+    },
+
+    {
+      "name": "reeti gowla",
+      "kritis": [
+        "Enna punniyam Seyadu",
+        "Nannoo Vidachi Kadalakura",
+        "Brindaavana Nilayer",
+        "Chera Rava Demira",
+        "Aadi Vinaayakam",
+        "SathguruSwaamikai"
+      ],
+      "notes": {
+        "aarohana": "S G2 R2 G2 M1 N2 N2 S",
+        "avarohana": "S N2 D2 M1 G2 M1 P M1 G2 R2 S"
+      }
+    },
+
+    {
+      "name": "bhairavi",
+      "kritis": [
+        "Viri Boni Chiru navvu",
+        "Govinda Ghataya",
+        "Alamelu Manga Nee Aluka",
+        "Baala Gopala",
+        "Neela Neerada Sareera"
+      ],
+      "notes": {
+        "aarohana": "S N2 D2 M1 G2 M1 P M1 G2 R2 S",
+        "avarohana": "S N2 D1 P M1 G2 R2 S"
+      }
+    },
+
+    {
+      "name": "mayaa malawa gowla",
+      "kritis": [
+        "Deva Deva SharanambiKe",
+        "Meru SamaaNa",
+        "DuduKooGala Ninne DoraKoduKu"
+      ],
+      "notes": {
+        "aarohana": "S G2 R2 G2 M1 N2 N2 S",
+        "avarohana": "S N2 D2 M1 G2 M1 P M1 G2 R2 S"
+      }
+     },
+
+    {
+      "name": "kedaaram",
+      "kritis": [
+        "Valachee Vachi yunna naapi",
+        "Ananda Natanaa Prakaasam"
+      ],
+      "notes": {
+        "aarohana": "S M1 G3 M1 P N3 S",
+        "avarohana": "S N3 P M1 G3 R2 S"
+      }
+    },
+
+    {
+      "name": "devaamruta varshini",
+      "kritis": [
+        "Evarani Nirnayinchee Ri Raa"
+      ],
+      "notes": {
+        "aarohana": "",
+        "avarohana": ""
+      }
+    },
+
+    {
+      "name": "hemavathi",
+      "kritis": [
+        "Sree KaantheeMathi Sankari Yuvati", 
+        "Ninne Nammithi Neeraja Dalal Nayana"
+      ],
+      "notes": {
+        "aarohana": "S R2 G2 M2 P D2 N2 S",
+        "avarohana": "S N2 D2 P M2 G2 R2 S"
+      }
+    },
+
+    {
+      "name": "hindolam",
+      "kritis": [
+        "Saamja VaraGamana",
+        "Neerada Sama Neela Krishnaa Yegi",
+        "Bhajere Gopalam"
+      ],
+      "notes": {
+        "aarohana": "S G2 M1 D1 N2 S",
+        "avarohana": "S N2 D1 M1 G2 S"
+      }
+    },
+
+    {
+      "name": "dwijavanthi",
+      "kritis": [
+        "AkhilaanDeswari"
+      ],
+      "notes": {
+        "aarohana": "S R2 G3 M1 P D2 S",
+        "avarohana": "S N2 D2 P M1 G3 M1 R2 G2 R2 S"
+      }
+    },
+
+    {
+      "name": "kaapi",
+      "kritis": [
+        "Parulanna Maata Namma Vaddu"
+      ],
+      "notes": {
+        "aarohana": "S R2 M1 P N3 S",
+        "avarohana": "S N2 D2 N2 P M1 G2 R2 S"
+      }
+    },
+
+    {
+      "name": "kalyani",
+      "kritis": [
+        "Nidhi Chaala Sukhama",
+        "Raamuni sannidhi",
+        "Etaavuna Nilakada",
+        "Dayaado ramgaa",
+        "Patita Paavana Amboruha SharaNe"
+      ],
+      "notes": {
+        "aarohana": "S R2 G3 M2 P D2 N3 S",
+        "avarohana": "S N3 D2 P M2 G3 R2 S"
+      }
+    },
+
+    {
+      "name": "kambhoji",
+      "kritis": [
+        "Mari Mari Ninne",
+        "Evari Maata Vinnaavo Raavo",
+        "Sri Subhranyaya",
+        "Sri Raghava",
+        "Emayya Raama"
+      ],
+      "notes": {
+        "aarohana": "S R2 G3 M1 P D2 S",
+        "avarohana": "S N2 D2 P M1 G3 R2 S"
+      }
+    },
+
+    {
+      "name": "kuntala varaali",
+      "kritis": [
+        "Deenuda Nenu Devudavu Neevu",
+        "Devi PaavaNi Shive Sharane"
+      ],
+      "notes": {
+        "aarohana": "S M1 P D2 N2 D2 S",
+        "avarohana": "S N2 D2 P M1 S"
+      }
+    },
+
+    {
+      "name": "hari kambhoji",
+      "kritis": [
+        "Annapoorne VisaLakshee"
+      ],
+      "notes": {
+        "aarohana": "S R2 G3 M1 P D2 N2 S",
+        "avarohana": "S N2 D2 P M1 G3 R2 S"
+      }
+    },
+
+    {
+      "name": "poorna chandrika",
+      "kritis": [
+        "Telisi Raama Chintana"
+      ],
+      "notes": {
+        "aarohana": "S R2 G3 M1 P D2 P S",
+        "avarohana": "S N3 P M1 R2 G3 M1 R2 S"
+      }
+    },
+
+    {
+      "name": "mukhari",
+      "kritis": [
+        "Kaaroo Baaru",
+        "Ksheenamai Tiruga",
+        "Sivaguru Naadanai",
+        "Chaaru Seele Priye (jampa)"
+      ],
+      "notes": {
+        "aarohana": "S R2 M1 P N2 D2 S",
+        "avarohana": "S N2 D1 P M1 G2 R2 S"
+      }
+    },
+
+    {
+      "name": "hamsaanandam",
+      "kritis": [
+        "Bantu Reeti KoluveeyaVayya"
+      ],
+      "notes": {
+        "aarohana": "S R2 M2 P N3 S",
+        "avarohana": "S N3 P M2 R2 S"
+      }
+    },
+
+    {
+      "name": "bahudaari",
+      "kritis": [
+        "Sadaa Nanda Taandavam"
+      ],
+      "notes": {
+        "aarohana": "S G3 M1 P D2 N2 S",
+        "avarohana": "S N2 P M1 G3 S"
+      }
+    },
+
+    {
+      "name": "aabhogi",
+      "kritis": [
+        "SabhaaPathiKi Veru Daivam"
+      ],
+      "notes": {
+        "aarohana": "S R2 G2 M1 D2 S",
+        "avarohana": "S D2 M1 G2 R2 S"
+      }
+    },
+
+    {
+      "name": "pantu varaali",
+      "kritis": [
+        "Shiva Shiva YanaRaaDa",
+        "RaghuVara Ninne",
+        "Apa Raama Bhakti Yento",
+        "Sambho MahaaDeva Sankara", 
+        "SaaKetha PuraMelaLeda Konnallu"
+      ],
+      "notes": {
+        "aarohana": "S R1 G3 M2 P D1 N3 S",
+        "avarohana": "S N3 D1 P M2 G3 R1 S"
+      }
+    },
+
+    {
+      "name": "deva gaandhari",
+      "kritis": [
+        "SabhaaPathiKi Veru Daivam"
+      ],
+      "notes": {
+        "aarohana": "S R2 M1 P D2 S",
+        "avarohana": "S N3 D2 N2 D2 P M1 G3 R2 S"
+      }
+    },
+
+    {
+      "name": "ananda bhairavi",
+      "kritis": [
+        "KheeraSagara Vihara",
+        "MariVere Gati EvvaRamma",
+        "SabhaaPathiKi Veru Daivam",
+        "Sama Gaana Priye Tamil"
+      ],
+      "notes": {
+        "aarohana": "S G2 R2 G2 M1 P D2 P S",
+        "avarohana": "S N2 D2 P M1 G2 R2 S"
+      }
+    },
+
+    {
+      "name": "shyama",
+      "kritis": [
+        "NimishaMaiNa Sree RaamaaYanaRaaDa"
+      ],
+      "notes": {
+        "aarohana": "",
+        "avarohana": ""
+      }
+    },
+
+    {
+      "name": "latangi",
+      "kritis": [
+        "Kanta JooduMi"
+      ],
+      "notes": {
+        "aarohana": "S R2 G3 M2 P D1 N3 S",
+        "avarohana": "S N3 D1 P M2 G3 R2 S"
+      }
+    },
+
+    {
+      "name": "kaanada",
+      "kritis": [
+        "Maama Mama Sadaa"
+      ],
+      "notes": {
+        "aarohana": "S R2 P G2 M1 D2 N2 S",
+        "avarohana": "S N2 P M1 G2 M1 R2 S"
+      }
+    },
+
+    {
+      "name": "yedukula kambhoji",
+      "kritis": [
+        "Eteeruga Nanu Daya ChoocheDavo"
+      ],
+      "notes": {
+        "aarohana": "S R2 M1 P D2 S",
+        "avarohana": "S N2 D2 P M1 G3 R2 S"
+      }
+    },
+
+    {
+      "name": "nattai",
+      "kritis": [
+        "Jaya Jayaswamin JayJaya",
+        "Gopaalaka Paahimaam Anisham  Gaayatri Venkatraghvan",
+        "Sarvam Eka Samakha MS"
+      ],
+      "notes": {
+        "aarohana": "S R3 G3 M1 P D3 N3 S",
+        "avarohana": "S N3 P M1 G3 M1 R3 S"
+      }
+    },
+
+    {
+      "name": "kamas",
+      "kritis": [
+       "Rama Jogi Mandoo",
+       "Santaana Gopaala Krishnan MupaasmaHe"
+      ],
+      "notes": {
+        "aarohana": "S M1 G3 M1 P D2 N2 S",
+        "avarohana": "S N2 D2 P M1 G3 R2 S"
+      }
+    },
+
+    {
+      "name": "yamuna kalyani",
+      "kritis": [
+        "Marakata Mani",
+        "Lambodaraaya Namaste",
+        "Ille Vaikhuntamu"
+      ],
+      "notes": {
+        "aarohana": "",
+        "avarohana": ""
+      }
+    },
+
+    {
+      "name": "varali",
+      "kritis": [
+        "Marakata Mani",
+        "Lambodaraaya Namaste",
+        "Ille Vaikhuntamu"
+      ],
+      "notes": {
+        "aarohana": "S G1 R1 G1 M2 P D1 N3 S",
+        "avarohana": "S N3 D1 P M2 G1 R1 S"
+      }
+    },
+
+    {
+      "name": "poorvi kalyani",
+      "kritis": [
+        "Gummani Yedi Sruthi", 
+        "Tillai Chidambaram Endraa Nalla Sruti Moodee",
+        "PaRaLoka SaaDhaNaMe",
+        "JnaaNaMoSaGa LeDaa" 
+      ],
+      "notes": {
+        "aarohana": "S R1 G3 M2 P D2 P S",
+        "avarohana": "S N3 D2 P M2 G3 R1 S"
+      }
+    },
+
+    {
+      "name": "jaya manohari",
+      "kritis": [
+        "Nee Bhakthi Bhagya Sudhaa"
+      ],
+      "notes": {
+        "aarohana": "",
+        "avarohana": ""
+      }
+    },
+
+    {
+      "name": "neelambari",
+      "kritis": [
+        "DeenuDa Nenu DevuDavui Neevu",
+        "Mani Moopura Dhaari Raja Gopala", 
+        "LaaliLooGave MaaPaali",
+        "Uyyaala LooGaVayya"
+      ],
+      "notes": {
+        "aarohana": "S G3 M1 P N3 S",
+        "avarohana": "S N3 P D2 N2 P M1 G3 R2 M1 G3 S"
+      }
+    },
+
+    {
+      "name": "suddha dhanyasi",
+      "kritis": [
+        "Enta VaaraLaina KaanTaa DaaSuLe",
+        "SubhrhamManyeNa RaaKshiToHam"
+      ],
+      "notes": {
+        "aarohana": "S G2 M1 P N2 S",
+        "avarohana": "S N2 P M1 G2 S"
+      }
+    },
+
+    {
+      "name": "mohana kalyani",
+      "kritis": [
+        "TaamaDam TagaaDayaa"
+      ],
+      "notes": {
+        "aarohana": "S R2 G3 P D2 S",
+        "avarohana": "S N3 D2 P M2 G3 R2 S"
+      }
+    },
+
+    {
+      "name": "vaachaspathi",
+      "kritis": [
+        "Kanta Choodumi",
+        "Paraatpara Parameswara"
+      ],
+      "notes": {
+        "aarohana": "",
+        "avarohana": ""
+      }
+    },
+
+    {
+      "name": "aarabhi",
+      "kritis": [
+        "SamaYaaniKi TaguMaataLaDeNe"
+      ],
+      "notes": {
+        "aarohana": "S R2 M1 P D2 S",
+        "avarohana": "S N3 D2 P M1 G3 R2 S"
+      }
+    },
+
+    {
+      "name": "sankarabharanam",
+      "kritis": [
+        "Enduku PeddaLaVale",
+        "Eduta NilaCheeTe NiluVu",
+        "Manavi CheKona Raada"
+      ],
+      "notes": {
+        "aarohana": "S R2 G3 M1 P D2 N3 S",
+        "avarohana": "S N3 D2 P M1 G3 R2 S"
+      }
+    },
+
+    {
+      "name": "dhanyasi",
+      "kritis": [
+        "TeliyaaLeru Raama BhaktiMaarGamu",
+        "Baagaayanayya Neemahima"
+      ],
+      "notes": {
+        "aarohana": "S G2 M1 P N2 S",
+        "avarohana": "S N2 D1 P M1 G2 R1 S"
+      }
+    },
+    {
+      "name": "dhenkua",
+      "kritis": [
+	"Kaama Dhenuka Aasraye"
+      ],
+      "notes": {
+        "aarohana": "S R1 G2 M1 P D1 N2 S",
+        "avarohana": "S N2 D1 P M1 G2 R1 S"
+      }
+    },
+
+    {
+      "name": "shanmukha priya",
+      "kritis": [
+        "ParamaPurusha Nirupa Maana"
+      ],
+      "notes": {
+        "aarohana": "S R2 G2 M2 P D1 N2 S",
+        "avarohana": "S N2 D1 P M2 G2 R2 S"
+      }
+    },
+
+    {
+      "name": "janaranjani",
+      "kritis": [
+        "VidaJaaLanuRaa NaaManaSu VinaRaa"
+      ],
+      "notes": {
+        "aarohana": "S R2 G3 M1 P D2 P N3 S",
+        "avarohana": "S D2 P M1 R2 S"
+      }
+    },
+
+    {
+      "name": "bilahari",
+      "kritis": [
+        "Sree MadhuRaapuri Viharini",
+        "ToliJANmamula jesi Duduku Telisenu Rama"
+      ],
+      "notes": {
+        "aarohana": "S P A S A RI G2",
+        "avarohana": "R G M P D N S"
+      }
+    },
+
+    {
+      "name": "gambhiranata",
+      "kritis": [
+        "Amma Ananda Daayini"
+      ],
+      "notes": {
+        "aarohana": "S G3 M1 P N3 S",
+        "avarohana": "S N3 P M1 G3 S"
+      }
+    },
+
+    {
+      "name": "devaranji",
+      "kritis": [
+        "Namaste Para Devate"
+      ],
+      "notes": {
+        "aarohana": "S M1 P D1 P N3 S",
+        "avarohana": "S N3 D1 P M1 S"
+      }
+    },
+
+    {
+      "name": "sarvasri",
+      "kritis": [
+        "Uma Sutam Namami"
+      ],
+      "notes": {
+        "aarohana": "S M1 P S",
+        "avarohana": "S P M1 S"
+      }
+    },
+
+    {
+      "name": "poorvi kalyani",
+      "kritis": [
+        "Gummani Yedi Sruthi"
+      ],
+      "notes": {
+        "aarohana": "S R1 G3 M2 P D2 P S",
+        "avarohana": "S N3 D2 P M2 G3 R1 S"
+      }
+    },
+
+    {
+      "name": "mohana",
+      "kritis": [
+        "Nanu Paalimpa Nadachi",
+	"EvarooRa Ninu Vina",
+	"Narasimhaa Gaccha Vara Puccha"
+      ],
+      "notes": {
+        "aarohana": "S R2 G3 P D2 S",
+        "avarohana": "S D2 P G3 R2 S"
+      }
+    }
+  ]
+};
